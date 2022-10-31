ddae11946f3cfac02e59e255535e533bb5eda9c9
Marcelo Galli <marcelolemesgalli@hotmail.com>
2022-10-30 22:38:16 -0300

    update
    update

--- stage08/src/controllers/UsersController.js
+++ stage08/src/controllers/UsersController.js
@@ -6,5 +6,19 @@ class UsersControlers {
         const {name, email, password} = request.body;
 
         const database = await sqliteConnection()
+
+        const checkUserExists = await database.get("SELECT * FROM users WHERE = email(?)", [email])
+
+        if(checkUserExists){
+            throw new AppError("User already exists!")
+        }
+
+        const hashedPassword = await hash(password, 10)
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword])
+
+        return response.status(201).json();
     }
 }
+
+module.exports = UsersControlers;

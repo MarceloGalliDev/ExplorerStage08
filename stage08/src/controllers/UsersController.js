98e7ee5523872d308d67722cee5d3f6ae551cc9c
Marcelo Galli <marcelolemesgalli@hotmail.com>
2022-10-30 22:24:45 -0300

    update
    update

--- stage08/src/controllers/UsersController.js
+++ stage08/src/controllers/UsersController.js
@@ -1,1 +1,10 @@
-    
+const { hash, compare } = require("bcryptjs")
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
+class UsersControlers {
+    async create(req, res){
+        const {name, email, password} = request.body;
+
+        const database = await sqliteConnection()
+    }
+}
